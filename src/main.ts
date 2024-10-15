efd5de4f4afe10ba20e9a0d4c9669ff9b1fa2353
Nayf <nayfserag5@gmail.com>
2024-10-15 17:18:59 +0300

    setup the basic for project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe, VersioningType , Logger } from '@nestjs/common';
+import { config } from './app/shared/module/config-module/config-module.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(config.getNumber('PORT'));
+  Logger.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
