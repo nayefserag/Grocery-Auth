d5bb32b557059192132b8f41ddcffd7eb2916e87
Nayf <nayfserag5@gmail.com>
2024-10-15 17:32:06 +0300

    setup db

---
+++ src/app/module/database/data-source.ts
@@ -0,0 +1,20 @@
+import { config } from 'src/app/shared/module/config-module/config-module.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: config.getString('DATABASE_HOST'),
+  port: config.getNumber('DATABASE_PORT'),
+  username: config.getString('DATABASE_USERNAME'),
+  password: config.getString('DATABASE_PASSWORD'),
+  database: config.getString('DATABASE_SCHEMA'),
+  migrationsTableName: 'migrations',
+  migrations: ['dist/app/modules/database/migrations/*.js'],
+  entities: ['dist/**/*.entity.js'],
+  synchronize: config.getString('DATABASE_SYNCHRONIZE') === 'true',
+  logging: config.getString('DATABASE_LOGGING') === 'true',
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
