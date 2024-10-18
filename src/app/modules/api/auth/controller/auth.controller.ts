75105935ebf96ecdf58ff868dcd67070d50b3f4e
Nayf <nayfserag5@gmail.com>
2024-10-19 00:23:17 +0300

    set tokens in headers

--- src/app/modules/api/auth/controller/auth.controller.ts
+++ src/app/modules/api/auth/controller/auth.controller.ts
@@ -24,14 +24,35 @@ export class AuthController {
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
-  async signup(@Body() signUpDto: SignupDto) {
-    return this.authService.signup(signUpDto);
+  async signup(@Body() signUpDto: SignupDto, @Response() res) {
+    const { access_token, refresh_token, user } =
+      await this.authService.signup(signUpDto);
+
+    res.setHeader('Authorization', `Bearer ${access_token}`);
+    res.setHeader('x-refresh-token', refresh_token); // Optionally, set refresh token in header
+
+    return res.status(HttpStatus.CREATED).json({
+      message: 'User signed up successfully',
+      user,
+      access_token,
+      refresh_token,
+    });
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
+  async login(@Body() loginDto: LoginDto, @Response() res) {
+    const { access_token, refresh_token } =
+      await this.authService.login(loginDto);
+
+    res.setHeader('Authorization', `Bearer ${access_token}`);
+    res.setHeader('x-refresh-token', refresh_token);
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Login successful',
+      access_token,
+      refresh_token,
+    });
   }
 
   @UseGuards(JwtAuthGuard)
@@ -44,14 +65,22 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Patch('profile')
   @HttpCode(HttpStatus.OK)
-  async updateProfile(@Request() req, @Body() body : SignupDto) {
+  async updateProfile(@Request() req, @Body() body: SignupDto) {
     return this.authService.updateProfile(req.user.userId, body);
   }
 
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
-  async refresh(@Body('refresh_token') refreshToken: string) {
-    return this.authService.refreshAccessToken(refreshToken);
+  async refresh(@Body('refresh_token') refreshToken: string, @Response() res) {
+    const { access_token } =
+      await this.authService.refreshAccessToken(refreshToken);
+
+    res.setHeader('Authorization', `Bearer ${access_token}`);
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Access token refreshed',
+      access_token,
+    });
   }
 
   @Post('forgot-password')
@@ -94,10 +123,9 @@ export class AuthController {
     return { message: 'Account deleted successfully' };
   }
 
-  
   @Post('complete-registration/:id')
   @HttpCode(HttpStatus.OK)
-  async completeRegistration(@Body() body : SignupDto, @Param('id') id: string) {
+  async completeRegistration(@Body() body: SignupDto, @Param('id') id: string) {
     return this.authService.updateProfile(body.id, body);
   }
 }
